3ce44993ebea04ba83e6b76e4730c6688f03f302
nboyse <natasha.boyse@digital.trade.gov.uk>
2021-11-23 13:20:32 +0000

    Setup of post and get functions

--- app.py
+++ app.py
@@ -7,6 +7,15 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+@app.route('/post', methods = ['POST'])
+def post_data():
+    # POST endpoint to capture domain/category/value metric
+    pass
+
+@app.route('/get', methods = ['GET'])
+def get_data():
+    # GET endpoint to show values for a given domain
+    pass
 
 if __name__ == '__main__':
     app.run()
